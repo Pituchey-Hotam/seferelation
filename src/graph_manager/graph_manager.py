fdd2baa25d723f224b253df836aa2e1474a0e2f3
Yosef Kaner <ykaner12@gmail.com>
2023-01-01 18:54:12 +0200

    graph add_edge

--- src/graph_manager/graph_manager.py
+++ src/graph_manager/graph_manager.py
@@ -11,8 +11,11 @@ class Graph:
             self.graph.add_node(ref)
         return self.graph.nodes[ref]
 
-    def add_edge(self, node1, node2, weight=None):
-        return self.graph.add_edge(node1, node2, weight=weight)
+    def add_edge(self, node1: str, node2: str, weight: int):
+        if node1 in self.graph and ndoe2 in self.graph[node1]:
+            self.graph[node1][node2] += weight
+        else:
+            self.graph.add_edge(node1, node2, weight=weight)
 
     def size(self):
         return len(self.graph.nodes)
