bfc47a4ae2925eb3d3f7b1550a51f63c4408e94e
Yosef Kaner <ykaner12@gmail.com>
2023-01-03 14:54:14 +0200

    add_node, fix add_edge weights

--- src/graph_manager/graph_manager.py
+++ src/graph_manager/graph_manager.py
@@ -11,11 +11,18 @@ class Graph:
             self.graph.add_node(ref)
         return self.graph.nodes[ref]
 
-    def add_edge(self, node1: str, node2: str, weight: int):
+    def add_node_type(self, node, node_type):
+        self.graph.add_node(node, type=node_type)
+
+    def add_edge(self, node1: str, node2: str, **attrs):
         if node1 in self.graph and ndoe2 in self.graph[node1]:
-            self.graph[node1][node2] += weight
+            for attr, value in attrs.items():
+                if attr in self.graph[node1][node2]:
+                    self.graph[node1][node2][attr] += value
+                else:
+                    self.graph[node1][node2][attr] = value
         else:
-            self.graph.add_edge(node1, node2, weight=weight)
+            self.graph.add_edge(node1, node2, **attrs)
 
     def size(self):
         return len(self.graph.nodes)
