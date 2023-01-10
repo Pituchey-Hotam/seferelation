0160fe9fab98963e004c9cd4e507aa6e708ebc90
Yosef Kaner <ykaner12@gmail.com>
2023-01-10 15:05:17 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="seferelation",
+    version="0.0.1",
+    packages=find_packages(),
+    url="https://github.com/ykaner/relation-project",
+    license="",
+    author="ykaner",
+    author_email="ykaner12@gmail.com",
+    description="Find relations in the jewish library",
+    install_requires=[
+        "tqdm",
+    ],
+)
