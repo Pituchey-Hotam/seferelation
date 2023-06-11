132f26c926afafa5bbf0ebba2d04e0a72eeb3346
Yosef Kaner <ykaner12@gmail.com>
2023-06-11 17:29:36 +0300

    Add a manual setupProxy.js

---
+++ webapp/seferelation/src/setupProxy.js
@@ -0,0 +1,12 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: 'http://localhost:3080',
+            changeOrigin: true,
+        })
+    );
+};
+
