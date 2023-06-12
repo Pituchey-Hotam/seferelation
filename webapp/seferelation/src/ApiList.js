72c74a55036b024bdd19bc7a5b8770a5cc1896e4
Yosef Kaner <ykaner12@gmail.com>
2023-06-12 21:31:44 +0300

    Use antd, thaks to GPT.

--- webapp/seferelation/src/ApiList.js
+++ webapp/seferelation/src/ApiList.js
@@ -1,40 +1,82 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
+import { Form, Input, Button, List, Typography } from 'antd';
 
+const { Item } = List;
 
-function ApiList() {
-    const [relations, setRelations] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
+class ApiList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput: { sefaria_link: '' },
+      relations: [],
+      searchTerm: ''
+    };
+  }
 
-    useEffect(() => {
-        axios.get(`/api/relations?sefaria_link=${searchTerm}`)
-            .then(response => setRelations(response.data.relations));
-    }, [searchTerm]);
+  componentDidMount() {
+    this.fetchRelations();
+  }
 
-    function handleSearchChange(event) {
-        setSearchTerm(event.target.value);
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.fetchRelations();
     }
+  }
+
+  fetchRelations = () => {
+    axios.get(`/api/relations?sefaria_link=${this.state.searchTerm}`)
+      .then(response => {
+        this.setState({ relations: response.data.relations || [] });
+      });
+  }
+
+  handleSearchChange = event => {
+    this.setState({
+      userInput: { sefaria_link: event.target.value }
+    });
+  }
+
+  handleSearchSubmit = event => {
+    this.setState({ searchTerm: this.state.userInput.sefaria_link });
+    event.preventDefault();
+  }
+
+  render() {
+    const { userInput, relations } = this.state;
 
     return (
-        <div>
-        <h1>חיפוש מקורות לימוד</h1>
-        <label>
-        Search relations with seferelation api (by @ykaner):
-        <p/>
-        בשביל לחפש העתיקו קישור מספריא לדבר שאותו אתם לומדים, לדוגמא: 
-        <p/>
-        https://www.sefaria.org.il/Genesis.1.1
-        <p/>
-        <input type="text" value={searchTerm} onChange={handleSearchChange} />
-        </label>
-        <ul>
-        {relations.map(item => (
-            <li key={item[0]}>{item[1]}: <a href={item[0]}> {item[0]}</a></li>
-        ))}
-        </ul>
-        </div>
+      <div>
+        <Typography.Title level={2}>חיפוש מקורות לימוד</Typography.Title>
+        <Typography.Paragraph>
+          Search relations with seferelation api (by @ykaner):
+          <br />
+          בשביל לחפש העתיקו קישור מספריא לדבר שאותו אתם לומדים, לדוגמא:
+          <br />
+          https://www.sefaria.org.il/Genesis.1.1
+        </Typography.Paragraph>
+
+        <Form onFinish={this.handleSearchSubmit}>
+          <Form.Item>
+            <Input value={userInput.sefaria_link} onChange={this.handleSearchChange} />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">Search</Button>
+          </Form.Item>
+        </Form>
+
+        <List
+          header={<Typography.Title level={3}>Results</Typography.Title>}
+          dataSource={relations}
+          renderItem={item => (
+            <Item>
+              <Typography.Text mark><a href={item[0]}>{item[1]}</a></Typography.Text> {item[0]}
+            </Item>
+          )}
+        />
+      </div>
     );
+  }
 }
 
 export default ApiList;
-
