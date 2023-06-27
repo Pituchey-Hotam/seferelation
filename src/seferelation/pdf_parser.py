58d8ecd7d5e4f33262e8365ece5195c2bb9532c7
Yosef Kaner <ykaner12@gmail.com>
2023-06-27 16:30:39 +0300

    Start parsing pdfs with sefaria

--- src/seferelation/pdf_parser.py
+++ src/seferelation/pdf_parser.py
@@ -1,5 +1,6 @@
-from types import List
+from typing import List
 
+import requests
 from pypdf import PdfReader
 import re
 
@@ -16,16 +17,40 @@ def _is_source_line(line: str) -> bool:
     return False
 
 
-def parse_pdf_sources(path: str) -> List[str]:
+def _filter_source_line(line: str) -> str:
+    idx = line.find(".")
+    return line[idx+1:]
+
+
+def parse_pdf_to_text_sources(path: str) -> List[str]:
     reader = PdfReader(path)
     text = []
     for page in reader.pages:
         text.append(page.extract_text(visitor_text=_visitor_body))
     
     lines = ''.join(text).splitlines()
-    source_lines = [line for line in lines if _is_source_line(line)]
+    source_lines = [_filter_source_line(line) for line in lines if _is_source_line(line)]
     return source_lines
 
 
+def parse_pdf_to_sefaria(path: str) -> List[str]:
+    source_text = parse_pdf_to_text_sources(path)
+    for source in source_text:
+        sefaria_ref = translate_source_to_sefaria(source)
+        print(f"souce:\t{source}\nsefaria:\t{sefaria_ref}")
+
+
 def translate_source_to_sefaria(heb_ref: str) -> str:
-    pass
+    url = f"https://www.sefaria.org/api/name/{heb_ref}"
+    response = requests.get(url).json()
+    if response["is_ref"] == True:
+        return response["ref"]
+    return "pasten"
+
+
+
+if __name__ == "__main__":
+    import sys
+    print(sys.argv)
+    parse_pdf_to_sefaria(sys.argv[1])
+
